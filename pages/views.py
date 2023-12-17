68c82bb27ebdb5cdac0a39372393c09b958ee14f
negar-toosi <144216039+negar-toosi@users.noreply.github.com>
2023-12-17 20:29:27 +0330

    send data with render

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'pages/index.html')
+    email = 'negar.tusi@gmail.com'
+    phone_number = '+98 902 745 8376'
+    name = 'Negar Toosi'
+    address = 'satarkhan Street, Tehran'
+    age = '21'
+    context = {'email': email,
+                'phone_number': phone_number,
+                'name': name,
+                'address': address,
+                'age': age}
+    return render(request, 'pages/index.html', context)
